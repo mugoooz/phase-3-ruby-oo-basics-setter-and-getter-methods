3f900acf7848a4d3cd8780ae470ab776319d3df7
Eric Mugo <mugoericpg20@gmail.com>
2023-05-18 19:00:58 +0300

    Debugged code

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
     def name=(name)
-        dogs_name = name
+        @dogs_name = name
     end
 
     def name
-        name
+        @dogs_name
     end
 
     def breed=(breed)
-        dogs_breed = breed
+        @dogs_breed = breed
     end
 
     def breed
-        breed
+        @dogs_breed
     end
-    
+
 
 end
